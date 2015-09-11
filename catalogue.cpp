d596e9c162ae67adcd1beb9af0ddd80b71e4e916
afontraille <afontraille@gmail.com>
2015-09-11 15:47:04 +0200

    activation de catalogue.cpp

--- catalogue.cpp
+++ catalogue.cpp
@@ -5,13 +5,7 @@
 
 catalogue::catalogue()
 {
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    maBase=new QSqlDatabase(db);
-    maBase->setHostName("172.16.63.111");
-    maBase->setDatabaseName("dbafontrailleNewWorld");
-    maBase->setUserName("afontraille");
-    maBase->setPassword("dTj124fs");
-    bool ok = maBase->open();
+
 }
 
 
