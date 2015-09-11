55c7b9e536cc74874f837915b216cba83e44399b
afontraille <afontraille@gmail.com>
2015-09-11 15:46:20 +0200

    Modification de la connexion à la base de données

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 int main(int argc, char *argv[])
 {
+    //connexion a la base de données
+    QSqlDatabase * baseCatalogue;
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    baseCatalogue = new QSqlDatabase(db);
+    baseCatalogue->setHostName("172.16.63.111");
+    baseCatalogue->setUserName("afontraille");
+    baseCatalogue->setDatabaseName("dbafontrailleNewWorld");
+    baseCatalogue->setPassword("dTj124fs");
+    baseCatalogue->open();
     QCoreApplication a(argc, argv);
     
     return a.exec();
