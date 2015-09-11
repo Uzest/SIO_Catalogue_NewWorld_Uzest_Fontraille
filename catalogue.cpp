6ec820c68692ccb81d16bb2a3bfcf2e762155400
Nicolas Uzest <nicolas.uzest@gmail.com>
2015-09-11 15:26:55 +0200

    Ajout connexion database

--- catalogue.cpp
+++ catalogue.cpp
@@ -1,5 +1,17 @@
 #include "catalogue.h"
+#include <QMessageBox>
+#include <QSqlRecord>
+#include <QSqlQuery>
 
 catalogue::catalogue()
 {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    maBase=new QSqlDatabase(db);
+    maBase->setHostName("172.16.63.111");
+    maBase->setDatabaseName("dbafontrailleNewWorld");
+    maBase->setUserName("afontraille");
+    maBase->setPassword("dTj124fs");
+    bool ok = maBase->open();
 }
+
+
