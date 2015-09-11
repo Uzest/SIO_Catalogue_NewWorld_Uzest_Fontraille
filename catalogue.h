8dda0619b43692d62be12a947decfa4812415c7f
Nicolas Uzest <nicolas.uzest@gmail.com>
2015-09-11 16:13:32 +0200

    Rajout dans le catalogue.h

--- catalogue.h
+++ catalogue.h
@@ -1,10 +1,19 @@
 #ifndef CATALOGUE_H
 #define CATALOGUE_H
 
+#include <QSqlDatabase>
+#include <QTextCodec>
+
 class catalogue
 {
 public:
     catalogue();
+
+
+private:
+Ui::Produit *ui;
+QSqlDatabase * baseCatalogue;
+
 };
 
 #endif // CATALOGUE_H
