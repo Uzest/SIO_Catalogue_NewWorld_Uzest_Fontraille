c3a533027f347c33e17feec3d1798f51b2121c70
Alexia <afontraille@gmail.com>
2016-06-14 12:45:03 +0200

    ajout des docs

--- PPE_Catalogue/pointDeVente.sql
+++
@@ -1,7 +0,0 @@
--- Jeu d'essais pour le point de vente
-delete from pointDeVente;
-INSERT INTO pointDeVente
-values
-	(1,'Gap','Pomme','Bouffier','Pierre','0665063363','pbouffier@gmail.com','rue du commerce',' ','05000','Gap'),
-	(2,'Marseille','Viande','Chauveau','Remy','0752163633','rchauveau@gmail.com','rue du cèdre',' ','13000','Marseille'),
-	(3,'Bollène','Pomme','Magat','Robin','0652558987','rmagat@gmail.com','rue du champ',' ','84200','Bollène');
